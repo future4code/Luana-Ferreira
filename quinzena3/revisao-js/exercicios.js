8c53686221395340b59830033ac852f65176fd8f
Mariana Rocha <luanaf09@gmail.com>
2021-06-18 14:36:45 -0300

    Adicionando o exercício 16 letra A

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -159,8 +159,12 @@ function anonimizaPessoa(pessoa) {
 
 // EXERCÍCIO 16A
 function maioresDe18(arrayDePessoas) {
+  const maiorDe18 = arrayDePessoas.filter((elemento) => {
+    return elemento.idade >= 18
+  })
+    return maiorDe18
+} 
   
-}
 // EXERCÍCIO 16B
 function menoresDe18(arrayDePessoas) {
 
